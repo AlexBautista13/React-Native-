9a9da62a865144d8e1c56d5bf6c832fb013d9d7a
alexb24 <lakers76767@gmail.com>
2019-12-17 20:50:17 -0800

    using state push number 5

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('alex');
+  const [person, setPerson] = useState({ name: 'coko', age: 36 });
+
+  const clickHandler = () => {
+    setName('chun-li');
+    setPerson({ name: 'Alex', age: 29 });
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name} </Text>
+      <Text>His name is {person.name} and his age is {person.age} </Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler} />
+      </View>
     </View>
   );
 }
@@ -15,5 +28,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer: {
+    marginTop:20,
   },
 });
